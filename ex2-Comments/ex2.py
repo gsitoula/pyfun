077ccf358d47941bf8d3b176a6f6903947079694
Gastón Sitoula <gaston@localhost.localdomain>
2017-07-25 13:35:05 -0300

    finished exercise 2

---
+++ ex2-Comments/ex2.py
@@ -0,0 +1,5 @@
+# Comments are like ghosts, they don't appear
+# when you execute the code
+print "This line appears when you execute it"
+# but this one doesn't, is ignored by the compiler
+print 'But, it you put # between "" it will appear'
