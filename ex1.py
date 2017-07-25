ef0f771e91dbbe5c575eddef2a4d6138a1ed29d1
Gastón Sitoula <gaston@localhost.localdomain>
2017-07-25 13:21:41 -0300

    first commit - excercise 1 finished

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print "Hello World!"
+print "Hello Again!"
+print "La concha de tu madre Allboys!"
+print "YEAH!"
+print "One small step for a man..."
+print "A huge step for Humanity"
+print "The Ace of Spades! The Ace of Spades!"
+# This is a comment, yeah!
+print "Another line added...Welcome new Line :)"
