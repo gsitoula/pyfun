af9e911358512d342f1455b9f96d0b33bd78b356
Gastón Sitoula <gaston@localhost.localdomain>
2017-07-25 13:25:43 -0300

    added ex1 and ex1-drill files to ex1 module

--- ex1/ex1-drill.py
+++ ex1/ex1-drill.py
@@ -5,3 +5,5 @@ print "YEAH!"
 print "One small step for a man..."
 print "A huge step for Humanity"
 print "The Ace of Spades! The Ace of Spades!"
+# This is a comment, yeah!
+print "Another line added...Welcome new Line :)"
